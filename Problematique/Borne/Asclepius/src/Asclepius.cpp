82a65e6d3c72516f45aa88d741c07683a3906b3c
g-lesssard <lesg2605@usherbrooke.ca>
2020-07-04 16:11:51 -0400

    Project setup

---
+++ Problematique/Borne/Asclepius/src/Asclepius.cpp
@@ -0,0 +1,25 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/git/Sherbrooke/APPs/S6-App5/Problematique/Borne/Asclepius/src/Asclepius.ino"
+/* App5 S6 
+ * Fait par: -Thierry Constantin (cont3301)
+ *           -Gabriel Lessard    (lesg2605)
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 7 "c:/git/Sherbrooke/APPs/S6-App5/Problematique/Borne/Asclepius/src/Asclepius.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
---
+++ Problematique/Borne/Asclepius/src/Asclepius.ino
@@ -0,0 +1,16 @@
+/* App5 S6 
+ * Fait par: -Thierry Constantin (cont3301)
+ *           -Gabriel Lessard    (lesg2605)
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
