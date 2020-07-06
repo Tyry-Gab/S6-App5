d56af5279e8aa511a123eafd04c3cc0ed04bd80a
g-lesssard <lesg2605@usherbrooke.ca>
2020-07-06 15:16:11 -0400

    Can now publish to MQTT

--- Laboratoire/Labo/serveur/events.js
+++ Laboratoire/Labo/serveur/events.js
@@ -1,15 +1,26 @@
 var Particle = require('particle-api-js');
+var MQTT = require('mqtt');
 var particle = new Particle();
 
-var CLIENT_ID="s6-app5-1560"
-var CLIENT_SECRET="b1debe855e3b169fffbee53b7e8ad352485a66d4"
 var DEVICE_NAME="e00fce68043174ec2c0c805e"
-
 var TOKEN="497689e3a6afb1af1762adff419e2a495741b927"
 
+var MQTTOptions = {
+  clientId: "clientId-cqnYsKIar3",
+  username:"Tyry-Gab",
+  password:"password",
+  clean:true
+}
 
-particle.getEventStream({ deviceId: DEVICE_NAME, auth: TOKEN }).then(function(stream) {
-  stream.on('event', function(data) {
-    console.log("Event: ", data);
+
+var MQTTClient = MQTT.connect("mqtt://broker.mqttdashboard.com", MQTTOptions);
+MQTTClient.on("connect",function(){	
+  MQTTClient.subscribe("Argon-Event");
+  particle.getEventStream({ deviceId: DEVICE_NAME, auth: TOKEN }).then(function(stream) {
+    stream.on('event', function(data) {
+      console.log("Event: ", data);
+      MQTTClient.publish("Argon-Event", data.data);
+    });
   });
-});
+})
+
