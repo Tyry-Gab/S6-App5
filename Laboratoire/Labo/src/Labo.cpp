98e63d7cd5258865dd63efcbe16b3e25db35f0e8
g-lesssard <lesg2605@usherbrooke.ca>
2020-07-06 10:39:23 -0400

    Added lab project

--- Laboratoire/Labo/src/Labo.cpp
+++ Laboratoire/Labo/src/Labo.cpp
@@ -3,25 +3,25 @@
 /******************************************************/
 
 #include "Particle.h"
-#line 1 "/home/casto/git/Sherbrooke/APPs/S6-App5/Problematique/Borne/Asclepius/src/Asclepius.ino"
-/* App5 S6 
- * Fait par: -Thierry Constantin (cont3301)
- *           -Gabriel Lessard    (lesg2605)
+#line 1 "/home/casto/git/Sherbrooke/APPs/S6-App5/Laboratoire/Labo/src/Labo.ino"
+/*
+ * Project Labo
+ * Description:
+ * Author:
+ * Date:
  */
-
 
 // setup() runs once, when the device is first turned on.
 void setup();
 void loop();
-#line 8 "/home/casto/git/Sherbrooke/APPs/S6-App5/Problematique/Borne/Asclepius/src/Asclepius.ino"
+#line 9 "/home/casto/git/Sherbrooke/APPs/S6-App5/Laboratoire/Labo/src/Labo.ino"
 void setup() {
   // Put initialization like pinMode and begin functions here.
-  Serial.begin(9600);
+
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  Serial.printlnf("Stuff");
-  delay(100);
+
 }
--- Laboratoire/Labo/src/Labo.ino
+++ Laboratoire/Labo/src/Labo.ino
@@ -1,18 +1,18 @@
-/* App5 S6 
- * Fait par: -Thierry Constantin (cont3301)
- *           -Gabriel Lessard    (lesg2605)
+/*
+ * Project Labo
+ * Description:
+ * Author:
+ * Date:
  */
-
 
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-  Serial.begin(9600);
+
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  Serial.printlnf("Stuff");
-  delay(100);
+
 }
