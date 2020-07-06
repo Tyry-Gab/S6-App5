c6abc9248194698718077c583fc4884713924cf8
g-lesssard <lesg2605@usherbrooke.ca>
2020-07-06 13:31:37 -0400

    Variable now accessible from cloud and script

--- Laboratoire/Labo/src/Labo.cpp
+++ Laboratoire/Labo/src/Labo.cpp
@@ -10,18 +10,25 @@
  * Author:
  * Date:
  */
-
-// setup() runs once, when the device is first turned on.
 void setup();
 void loop();
-#line 9 "/home/casto/git/Sherbrooke/APPs/S6-App5/Laboratoire/Labo/src/Labo.ino"
+#line 7 "/home/casto/git/Sherbrooke/APPs/S6-App5/Laboratoire/Labo/src/Labo.ino"
+int g_Counter = 0;
+// setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-
+  Particle.variable("counter", g_Counter);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-
+  if(g_Counter < 100){
+    g_Counter++;
+  }
+  else
+  {
+    g_Counter = 0;
+  }
+  
 }
--- Laboratoire/Labo/src/Labo.ino
+++ Laboratoire/Labo/src/Labo.ino
@@ -4,15 +4,22 @@
  * Author:
  * Date:
  */
-
+int g_Counter = 0;
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-
+  Particle.variable("counter", g_Counter);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-
+  if(g_Counter < 100){
+    g_Counter++;
+  }
+  else
+  {
+    g_Counter = 0;
+  }
+  
 }
